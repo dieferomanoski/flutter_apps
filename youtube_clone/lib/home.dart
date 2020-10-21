4c1baf6c61851dda574d766d65ae5b44469e6000
Dieferson Romanoski <diefe-ki@outlook.com>
2020-10-21 15:11:36 -0300

    youtube app created

---
+++ youtube_clone/lib/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Youtube'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: (){
+              
+            }
+            ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+
+            }
+            ),
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: (){
+
+            }
+            )
+        ],
+      ),
+      body: Container(),
+    );;
+  }
+}
