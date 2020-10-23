398199f16723b41277875ac6f107402945d7139e
Dieferson Romanoski <diefe-ki@outlook.com>
2020-10-23 15:38:30 -0300

    youtube app finalized

--- youtube_clone/lib/home.dart
+++ youtube_clone/lib/home.dart
@@ -1,38 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/custom_search_delegate.dart';
+import 'package:youtube_clone/telas/biblioteca.dart';
+import 'package:youtube_clone/telas/em_alta.dart';
+import 'package:youtube_clone/telas/inicio.dart';
+import 'package:youtube_clone/telas/inscricao.dart';
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
+// 
+
 class _HomeState extends State<Home> {
+  int _bottomIndex = 0;
+  String _resultado = "";
+
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> telas = [
+      Inicio(_resultado),
+      EmAlta(),
+      Inscricao(),
+      Biblioteca(),
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Youtube'),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.grey[600]),
+        title: Image.asset(
+          "images/youtube.png",
+          width: 98,
+          height: 22,
+        ),
         actions: [
+          IconButton(icon: Icon(Icons.videocam), onPressed: () {}),
           IconButton(
-            icon: Icon(Icons.videocam),
-            onPressed: (){
-              
-            }
-            ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: (){
-
-            }
-            ),
-          IconButton(
-            icon: Icon(Icons.account_circle),
-            onPressed: (){
-
-            }
-            )
+              icon: Icon(Icons.search),
+              onPressed: () async {
+                String res = await showSearch(
+                    context: context, delegate: CustomSearchDelegate());
+                setState(() {
+                  _resultado = res;
+                });
+              }),
+          IconButton(icon: Icon(Icons.account_circle), onPressed: () {})
         ],
       ),
-      body: Container(),
-    );;
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Center(child: telas[_bottomIndex]),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _bottomIndex,
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+        onTap: (index) {
+          setState(() {
+            _bottomIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(label: "Início", icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: "Em alta", icon: Icon(Icons.whatshot)),
+          BottomNavigationBarItem(
+              label: "Inscrições", icon: Icon(Icons.subscriptions)),
+          BottomNavigationBarItem(
+              label: "Biblioteca", icon: Icon(Icons.folder)),
+        ],
+      ),
+    );
+    ;
   }
 }
