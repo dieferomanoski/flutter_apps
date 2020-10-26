61cfbd11b0f77c873013ca6d752958b43d5cd8c3
Dieferson Romanoski <diefe-ki@outlook.com>
2020-10-26 16:36:06 -0300

    app lista tarefas finalized

---
+++ lista_de_tarefas/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Lista de tarefas",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
